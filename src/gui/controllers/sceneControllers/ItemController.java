8287489da32a7238f0d7a0c015d26c9273d65a1e
Kjub <jakub.sovis@gmail.com>
2019-01-29 20:59:09 +0100

    small changes

--- src/gui/controllers/sceneControllers/ItemController.java
+++ src/gui/controllers/sceneControllers/ItemController.java
@@ -19,8 +19,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.ImageView;
